bf03fa80076a37b45af456d09fd0e3e7bcb989da
PAIA-Kylin@JetsonOrinNano <kylingithubdev@gmail.com>
2023-09-28 11:28:26 +0800

    fix : update init bash

--- scripts/create_from_boilerplate.bash
+++ scripts/create_from_boilerplate.bash
@@ -2,7 +2,7 @@ export PROJECT_NAME="new_project"
 export PROJECT_URL="new_project_url"
 git clone ssh://git@paia-tech.synology.me:8222/pros/pros-boilerplate.git $PROJECT_NAME
 cd $PROJECT_NAME
-git remote update origin $PROJECT_URL
-git branch -M main
-git push -uf origin main
-
+git remote rename origin old-origin
+git remote add origin $PROJECT_URL
+git tag | xargs git tag -d
+git push -u origin --all

4d8f7d77cd187ac902800f0c7bae601db5953749
paia_on_JetOrinNano <paia@jet_orin_nano_2.dev>
2023-11-14 09:58:08 +0000

    feat:add arm control

--- src/pros_car_py/pros_car_py/carA_keyboard.py
+++ src/pros_car_py/pros_car_py/carA_keyboard.py
@@ -8,6 +8,8 @@ import orjson
 from std_msgs.msg import String
 import curses
 import threading
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectoryPoint
 
 class CarAKeyboardController(Node):
     def __init__(self, stdscr):
@@ -28,6 +30,9 @@ class CarAKeyboardController(Node):
             DeviceDataTypeEnum.car_A_control,# topic name
             10
         )
+        self.joint_trajectory_publisher_ = self.create_publisher(
+            JointTrajectoryPoint, 'joint_trajectory_point', 10)
+        self.joint_pos = [1.57, 1.57, 1.57, 1.57, 1.0]
         self.stdscr = stdscr
         curses.noecho()
         curses.raw()
@@ -80,10 +85,32 @@ class CarAKeyboardController(Node):
                         self.handle_key_s()
                     elif c == ord('d'):
                         self.handle_key_d()
-
+                    elif c == ord('z'):
+                        self.handle_key_z()
+                    elif c == ord('i'):
+                        self.handle_key_i()
+                    elif c == ord('j'):
+                        self.handle_key_j()
+                    elif c == ord('k'):
+                        self.handle_key_k()
+                    elif c == ord('l'):
+                        self.handle_key_l()
+                    elif c == ord('u'):
+                        self.handle_key_u()
+                    elif c == ord('o'):
+                        self.handle_key_o()
+                    elif c == ord('y'):
+                        self.handle_key_y()
+                    elif c == ord('h'):
+                        self.handle_key_h()
                     elif c == ord('q'):  # Exit on 'q'
+                        self._direction =90 # degree
+                        self._vel = 0 # rad/s
+                        self._pub_control()
+
                         break
                     self._pub_control()
+                    self.pub_arm()
                 else:
                     self.print_basic_info(ord(' '))
                     time.sleep(0.01)
@@ -110,6 +137,9 @@ class CarAKeyboardController(Node):
 
         # 
         self.stdscr.move(4, 0)
+        self.stdscr.addstr(f"Arm pos : {self.joint_pos}")
+        self.stdscr.move(5, 0)
+
         # self.get_logger().debug(f"{self.key_in_count:5d} Key '{chr(key)}' pressed!")
 
     def handle_key_w(self):
@@ -137,9 +167,64 @@ class CarAKeyboardController(Node):
         self.stdscr.addstr(f"car turn right")
         self._direction =105 # degree
         pass
+    def handle_key_z(self):
+        self.stdscr.addstr(f"stop car")
+        self._direction =90 # degree
+        self._vel = 0 # rad/s
+        pass
 
-    
+    def handle_key_i(self):
+        self.stdscr.addstr(f"arm rift up")
+        self.joint_pos[2]+=0.05
+        
+        pass
 
+    def handle_key_j(self):
+        self.stdscr.addstr(f"arm turn left")
+        self.joint_pos[0]-=0.05
+
+        pass
+
+    def handle_key_k(self):
+        self.stdscr.addstr(f"arm rift down")
+        self.joint_pos[2]-=0.05
+
+        pass
+
+    def handle_key_l(self):
+        self.stdscr.addstr(f"arm turn right")
+        self.joint_pos[0]+=0.05
+
+        pass
+
+    def handle_key_u(self):
+        self.stdscr.addstr(f"arm j4 rotate left")
+        self.joint_pos[3]-=0.05
+
+        pass
+
+    def handle_key_o(self):
+        self.stdscr.addstr(f"arm j4 rotate right")
+        self.joint_pos[3]+=0.05
+        pass
+
+    def handle_key_y(self):
+        self.stdscr.addstr(f"arm catch!")
+        self.joint_pos[4]=1.57
+        pass
+
+    def handle_key_h(self):
+        self.stdscr.addstr(f"arm release!")
+        self.joint_pos[4]=1.0
+
+
+        pass
+    def pub_arm(self):
+        msg = JointTrajectoryPoint()
+        msg.positions = self.joint_pos  # Replace with actual desired positions
+        msg.velocities = [0.0, 0.0, 0.0, 0.0, 0.0] # Replace with actual desired velocities
+        # You can set other fields of the JointTrajectoryPoint message similarly.
+        self.joint_trajectory_publisher_.publish(msg)
 # ... Rest of your code, e.g. initializing rclpy and running the node
 
 def main(args=None):

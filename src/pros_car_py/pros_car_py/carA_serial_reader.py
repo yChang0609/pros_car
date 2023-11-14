2b48a195231e517a7bc6b64d47c3b60b2856aaef
paia_on_JetOrinNano <paia@jet_orin_nano_2.dev>
2023-11-14 07:48:50 +0000

    refac:update car reader

--- src/pros_car_py/pros_car_py/carA_serial_reader.py
+++ src/pros_car_py/pros_car_py/carA_serial_reader.py
@@ -1,5 +1,7 @@
 from pros_car_py.env import SERIAL_DEV_DEFAULT
-from pros_car_py.car_models import TwoWheelAndServoState
+from .car_models import *
+# from .car_models import DeviceDataTypeEnum
+# from pros_car_py.car_models import DeviceDataTypeEnum
 import rclpy
 from rclpy.node import Node
 import orjson
@@ -7,43 +9,50 @@ from std_msgs.msg import String
 from serial import Serial
 from rclpy.duration import Duration
 
-class CarAStatePublisher(Node):
+class CarASerialReader(Node):
     def __init__(self):
-        super().__init__('car_a_state_publisher')
+        super().__init__('car_a_serial_reader')
 
         serial_port = self.declare_parameter('serial_port', SERIAL_DEV_DEFAULT).value
         self._serial = Serial(serial_port, 115200, timeout=0)
 
         self.publisher = self.create_publisher(
             String,
-            'carA_state',
+            DeviceDataTypeEnum.car_A_state,
             10
         )
 
         # Create a timer to read from the serial port and publish state every 100 ms
         self.timer = self.create_timer(0.01, self.timer_callback)
         self.log_interval = Duration(seconds=1)  # Log every 1 seconds
-
+        current_time = self.get_clock().now()
+        self.last_log_time = current_time
 
     def timer_callback(self):
         if self._serial.in_waiting:
             incoming_data = self._serial.readline()
-            self.get_logger().info(f'Receive from car esp32: {incoming_data}')
+            current_time = self.get_clock().now()
+            if current_time - self.last_log_time >= self.log_interval:    
+                self.get_logger().info(f'Receive from car esp32: {incoming_data}')
+                self.last_log_time = current_time
 
             try:
                 # Assuming the incoming data is already in the required JSON format
                 
                 state_msg = String()
                 # validation should be customized
-                state_data =TwoWheelAndServoState(**orjson.loads(incoming_data))
-                state_msg.data = orjson.dumps({"type":"CarA_State","data":incoming_data.decode()}).decode()
+                state_data =dict(CarAState(**orjson.loads(incoming_data)))
+                state_msg.data = orjson.dumps(
+                    dict(DeviceData(type=DeviceDataTypeEnum.car_A_state,
+                        data=state_data))
+                    ).decode()
                 self.publisher.publish(state_msg)
             except orjson.JSONDecodeError as e:
                 self.get_logger().error(f'JSON decode error: {e}')
 
 def main(args=None):
     rclpy.init(args=args)
-    car_a_state_publisher = CarAStatePublisher()
+    car_a_state_publisher = CarASerialReader()
     rclpy.spin(car_a_state_publisher)
     car_a_state_publisher.destroy_node()
     rclpy.shutdown()
--- src/pros_car_py/pros_car_py/car_models.py
+++ src/pros_car_py/pros_car_py/car_models.py
@@ -1,7 +1,54 @@
+from enum import Enum, auto
 from typing import List
 import pydantic
 
 CARA_CONTROL = 'CarA_Control'
+
+
+class StringEnum(str, Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+    def __eq__(self, other):
+        if isinstance(other, StringEnum):
+            return self.value == other.value
+        elif isinstance(other, str):
+            return self.value == other
+
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return self.value
+
+    def __hash__(self):
+        return hash(self.value)
+
+class DeviceDataTypeEnum(StringEnum):
+    car_A_state = auto()
+    car_A_control = auto()
+    car_B_state = auto()
+    car_B_control = auto()
+    car_C_state = auto()
+    car_C_control = auto()
+    car_D_state = auto()
+    car_D_control = auto()
+    
+class DeviceData(pydantic.BaseModel):
+    type:DeviceDataTypeEnum
+    data:dict
+
+class CarAState(pydantic.BaseModel):
+    vels:List[float]=[]
+    encoders:List[int]=[]
+    direction:int
+
+class CarAControl(pydantic.BaseModel):
+    target_vel:List[float]=[]
+    direction:int=90
+
 class TwoWheelAndServoControlSignal(pydantic.BaseModel):
     target_vel:List[float]=[]
     direction:int=None

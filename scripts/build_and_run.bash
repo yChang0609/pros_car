eeae91a758f461e8b5d845e4b4f3a950f587461a
paia_on_JetOrinNano <paia@jet_orin_nano_2.dev>
2023-11-17 02:24:41 +0000

    feat:add launch file

--- scripts/build_and_run.bash
+++ scripts/build_and_run.bash
@@ -1,5 +1,6 @@
 colcon build --event-handlers console_direct+ --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install 
 source install/setup.bash
-ros2 run pros_car_py carA_keyboard
+# ros2 run pros_car_py carA_keyboard
 # ros2 run pros_car_py carA_random
 # ros2 run pros_car_py carA_reader
+ros2 launch pros_car_py carA_random.launch.py

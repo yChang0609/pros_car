129ed34f38ee0b3beafa6e24f5e9e4be92a18102
otischung <my072814638@gmail.com>
2023-11-20 12:09:42 +0800

    [FEAT] Add car A back
    Fix the merge bug when merging the d_dev

---
+++ src/pros_car_py/pros_car_py/carA_serial_writer.py
@@ -0,0 +1,57 @@
+from pros_car_py.env import SERIAL_DEV_DEFAULT
+from pros_car_py.car_models import *
+import rclpy
+from rclpy.node import Node
+import orjson
+from std_msgs.msg import String
+from serial import Serial
+
+
+class CarAControlSubscriber(Node):
+    def __init__(self):
+        super().__init__('car_a_control_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            DeviceDataTypeEnum.car_A_control,  # topic name
+            self.listener_callback,
+            10
+        )
+        serial_port = self.declare_parameter('serial_port', SERIAL_DEV_DEFAULT).value
+
+        self.subscription  # prevent unused variable warning
+        self._serial = Serial(serial_port, 115200, timeout=0)
+
+    def listener_callback(self, msg):
+        try:
+            control_data = orjson.loads(msg.data)
+            # TODO use more clear method to write
+            # TODO divide serial data and data validation
+            if control_data.get('type') == DeviceDataTypeEnum.car_A_control:
+                self.process_control_data(CarAControl, control_data.get('data', {}))
+        except orjson.JSONDecodeError as e:
+            self.get_logger().error('JSON decode error: {}'.format(e))
+        except KeyError as e:
+            self.get_logger().error('Missing key in JSON data: {}'.format(e))
+
+    def process_control_data(self, type_cls, data: dict):
+        # Process the control data as needed
+        # direction = data.get('direction')
+        # target_vel = data.get('target_vel')
+        # TODO should be customized
+        control_signal = type_cls(**data)
+
+        self._serial.write(orjson.dumps(dict(control_signal), option=orjson.OPT_APPEND_NEWLINE))
+        # self.get_logger().info(f'Received type:{type(data)} data: {data}')
+        self.get_logger().info(f'Received {control_signal}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    car_a_control_subscriber = CarAControlSubscriber()
+    rclpy.spin(car_a_control_subscriber)
+    car_a_control_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()

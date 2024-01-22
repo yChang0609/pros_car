0d1c8d0c6e42c3c7a1b719029b3e06d827745bdd
otischung <my072814638@gmail.com>
2024-01-22 21:20:26 +0800

    [FIX] Use mount to open src folder

--- car_control_4.sh
+++ car_control_4.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --device=/dev/usb_front_wheel --device=/dev/usb_rear_wheel --network host --env-file ./.env ghcr.io/otischung/pros_car:latest /bin/bash
+docker run -it --rm -v "$(pwd)/src:/src" --device=/dev/usb_front_wheel --device=/dev/usb_rear_wheel --network host --env-file ./.env ghcr.io/otischung/pros_car:latest /bin/bash

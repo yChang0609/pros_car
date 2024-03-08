c39244d7c495afc96ec2d87c192bf1f3f1958e4b
otischung <my072814638@gmail.com>
2024-03-08 15:38:14 +0800

    [FIX] Add normal exit

--- src/pros_car_py/pros_car_py/cv_bridge.py
+++ src/pros_car_py/pros_car_py/cv_bridge.py
@@ -4,7 +4,7 @@ import numpy as np
 import rclpy
 import sys
 import threading
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage, Image
 
@@ -56,37 +56,37 @@ class NodeVideoReader(Node):
         # )
         self.video_writer = None
         self.cv_image = None
-        print("FINISH constructor", file=sys.stderr)
 
     def image_callback(self, msg):
-        print("REACH image callback", file=sys.stderr)
         try:
             self.cv_image = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="bgr8")
             # self.cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-            # print(self.cv_image, file=sys.stderr)
-
             cv2.imshow('Robot Arm', self.cv_image)
             key = cv2.waitKey(1)
+            if key == 27:  # esc
+                cv2.destroyAllWindows()
+                self.destroy_node()
+                quit(0)
 
-            if self.video_writer is None:
-                self.init_video_writer(self.cv_image)
-            self.video_writer.write(self.cv_image)
+            # if self.video_writer is None:
+            #     self.init_video_writer(self.cv_image)
+            # self.video_writer.write(self.cv_image)
         except CvBridgeError as e:
             self.get_logger().error('CvBridge Error: %s' % str(e))
         except Exception as e:
             self.get_logger().error('Error processing image: %s' % str(e))
 
-    def init_video_writer(self, image):
-        try:
-            height, width, _ = image.shape
-            video_format = 'mp4'  # or any other video format supported by OpenCV
-            video_filename = 'output_video.' + video_format
-            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            fps = 30  # Frames per second
-            self.video_writer = cv2.VideoWriter(video_filename, fourcc, fps, (width, height))
-        except Exception as e:
-            self.get_logger().error('Error initializing video writer: %s' % str(e))
+    # def init_video_writer(self, image):
+    #     try:
+    #         height, width, _ = image.shape
+    #         video_format = 'mp4'  # or any other video format supported by OpenCV
+    #         video_filename = 'output_video.' + video_format
+    #         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    #         fps = 30  # Frames per second
+    #         self.video_writer = cv2.VideoWriter(video_filename, fourcc, fps, (width, height))
+    #     except Exception as e:
+    #         self.get_logger().error('Error initializing video writer: %s' % str(e))
 
     def destroy_node(self):
         if self.video_writer is not None:

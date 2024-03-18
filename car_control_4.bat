34cacb066a19d3be2a1d6dc228de1bfcc9b5e3e5
otischung <my072814638@gmail.com>
2024-03-18 21:43:52 +0800

    [FIX] Fix docker network bridge name

--- car_control_4.bat
+++ car_control_4.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run -it --rm -v "%cd%/src:/workspaces/src" -v "%cd%/FUNAI_digitaltwin_avoidance:/workspaces/FUNAI_digitaltwin_avoidance" --network pros_app_my_bridge_network --env-file ./.env ghcr.io/otischung/pros_ai_image:latest /bin/bash
+docker run -it --rm -v "%cd%/src:/workspaces/src" -v "%cd%/FUNAI_digitaltwin_avoidance:/workspaces/FUNAI_digitaltwin_avoidance" --network scripts_my_bridge_network --env-file ./.env ghcr.io/otischung/pros_ai_image:latest /bin/bash

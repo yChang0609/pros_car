62e2713aa0102aef03097710d49b00d45bddc4c1
otischung <my072814638@gmail.com>
2024-05-19 10:34:16 +0800

    [FIX] Add #!/bin/bash

--- car_control_2.sh
+++ car_control_2.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 docker run -it --rm -v "$(pwd)/src:/workspaces/src" --network scripts_my_bridge_network --device=/dev/usb_rear_wheel --device=/dev/usb_robot_arm  --env-file ./.env ghcr.io/otischung/pros_ai_image:latest /bin/bash

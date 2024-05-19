bb50bd6c51071811b7968bf1a408bef534419315
otischung <my072814638@gmail.com>
2024-05-19 10:37:32 +0800

    [MERGE] Use the newer version in main branch

--- car_control_4.sh
+++ car_control_4.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-docker run -it --rm -v "$(pwd)/src:/workspaces/src" --network scripts_my_bridge_network --device=/dev/usb_front_wheel --device=/dev/usb_rear_wheel --device=/dev/usb_robot_arm  --env-file ./.env ghcr.io/otischung/pros_ai_image:latest /bin/bash
+# 初始化device參數
+device_options=""
+
+# 檢查設備動態加入
+if [ -e /dev/usb_front_wheel ]; then
+    device_options+=" --device=/dev/usb_front_wheel"
+fi
+
+if [ -e /dev/usb_rear_wheel ]; then
+    device_options+=" --device=/dev/usb_rear_wheel"
+fi
+
+if [ -e /dev/usb_robot_arm ]; then
+    device_options+=" --device=/dev/usb_robot_arm"
+fi
+
+# 建構完整指令
+docker run -it --rm -v "$(pwd)/src:/workspaces/src" --network scripts_my_bridge_network $device_options --env-file ./.env ghcr.io/otischung/pros_ai_image:latest /bin/bash

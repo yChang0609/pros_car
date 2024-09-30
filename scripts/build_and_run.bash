0371e782dcedad429e5c23519698dda889197f18
otischung <my072814638@gmail.com>
2024-09-30 15:56:00 +0800

    [REFACTOR] Remove redundant files
    We've removed redundant files in this commit.
    1. Docker related
            deleted:    .devcontainer/devcontainer.json.template
            deleted:    .dockerignore
            deleted:    requirements.txt
            deleted:    scripts/build.bash
            deleted:    scripts/build_and_run.bash
            deleted:    scripts/create_from_boilerplate.bash
            deleted:    scripts/docker_build_and_push.bash
            deleted:    scripts/docker_build_and_run.bash
            deleted:    scripts/docker_buildx.bash
            deleted:    .github/workflows/docker_publish_ghcr.yaml
    2. Submodule: FUNAI_digitaltwin_avoidance
            deleted:    .gitmodules
            deleted:    FUNAI_digitaltwin_avoidance
    3. mediapipe related
            deleted:    mediapipe.sh
    	deleted:    src/pros_car_py/cv_bridge.py
    4. Old car model's launch file
            deleted:    src/pros_car_py/launch/carA_driver.launch.py
            deleted:    src/pros_car_py/launch/carA_random.launch.py
            modified:   src/pros_car_py/setup.py

--- scripts/build_and_run.bash
+++
@@ -1,7 +0,0 @@
-colcon build --event-handlers console_direct+ --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install 
-source install/setup.bash
-# ros2 run pros_car_py carA_keyboard
-# ros2 run pros_car_py carA_random
-# ros2 run pros_car_py carA_reader
-# ros2 launch pros_car_py carA_driver.launch.py
-ros2 launch pros_car_py carA_random.launch.py
